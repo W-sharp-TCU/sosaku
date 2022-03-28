12e8a8c3633e72df9c6037bff622944df2a82c79
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-03-29 03:51:32 +0900

    Add splash screen and restrict device orientations.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:sosaku/l10n/l10n.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  //向き指定
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight, // 横固定
+  ]);
   runApp(const MyApp());
 }
 
@@ -12,17 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: L10n.of(context)!.helloWorld,
+      title: "想咲 - ソウサク -",
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
