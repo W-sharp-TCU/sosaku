5a963e9270baf84b4022822a2df359f0d6c6430a
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-04-05 00:40:02 +0900

    マイナーチェンジ

--- lib/Title/Controller_title_SlideShowController.dart
+++ lib/Title/Controller_title_SlideShowController.dart
@@ -3,7 +3,7 @@ import 'package:sosaku/Title/Provider_title_TitleScreenProvider.dart';
 
 class SlideShowController {
   final int _interval = 3000; // [ms]
-  final List<String> _imagePaths = ["assets/drawable/Title/splash.png"];
+  final List<String> _imagePaths = ["assets/drawable/Splash/splash.png"];
 
   late TitleScreenProvider _target;
 
@@ -17,10 +17,10 @@ class SlideShowController {
 
   }
 
-  Future<void> changeImage() async {
-    await precacheImage(const AssetImage(_imagePaths[0]), context);
+  Future<void> changeImage(BuildContext context) async {
+    await precacheImage(AssetImage(_imagePaths[0]), context);
     _target.mBGImagePath = _imagePaths[0];
-    Future.delayed(const Duration(milliseconds: _interval));
+    Future.delayed(Duration(milliseconds: _interval));
   }
 
 }
