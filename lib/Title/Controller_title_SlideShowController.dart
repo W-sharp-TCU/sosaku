6e8f6e7af79e885ac77ebf43a9aff5924a2f8e36
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-04-19 11:49:57 +0900

    Fixed issue that assets could not be loaded on Android devices, changed app's label name & added sounds/ & drawable/LoadingProgress directories.

--- lib/Title/Controller_title_SlideShowController.dart
+++ lib/Title/Controller_title_SlideShowController.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:sosaku/Title/Interface_title_SlideShowInterface.dart';
 
-
-
 class SlideShowController {
   final int _interval = 5000; // [ms]
-  final List<String> _imagePaths = ["drawable/Title/Ocean.jpg",
-                                    "drawable/Title/Lion.jpg",
-                                    "drawable/Title/default.jpg"];
+  final List<String> _imagePaths = [
+    "assets/drawable/Title/Ocean.jpg",
+    "assets/drawable/Title/Lion.jpg",
+    "assets/drawable/Title/default.jpg"
+  ];
   SlideShowInterface? _target;
   int _i = 0;
-
 
   /// Start changing images in [_interval] milli seconds.
   /// This function can be called in build() of Widget class repeatedly.
@@ -19,23 +18,20 @@ class SlideShowController {
   /// @param target : specify the instance of [ChangeNotifier] which extends
   /// [SlideShowInterface].
   void start(BuildContext context, SlideShowInterface target) {
-    if (_target==null) {
+    if (_target == null) {
       _target = target;
       _threadLoop(context);
     }
   }
-  
+
   /// Stop changing.
   void stop() {
     _target = null;
   }
 
-  
-  
   Future<void> _threadLoop(BuildContext context) async {
     _changeImage(context).then((value) {
-      Future.delayed(Duration(milliseconds: _interval), ()
-      {
+      Future.delayed(Duration(milliseconds: _interval), () {
         if (_target != null) {
           _threadLoop(context);
         } else {
@@ -47,14 +43,13 @@ class SlideShowController {
 
   Future<void> _changeImage(BuildContext context) async {
     await precacheImage(AssetImage(_imagePaths[_i]), context);
-    if (_i != _imagePaths.length-1) {
+    if (_i != _imagePaths.length - 1) {
       _i++;
     } else {
       _i = 0;
     }
-    if (_target!=null) {
+    if (_target != null) {
       _target!.setImage(_imagePaths[_i]);
     }
   }
-
 }
