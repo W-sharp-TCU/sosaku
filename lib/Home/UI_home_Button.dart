f60e7792ca12854b95f93ed8719885b2597f3b04
Keisuke-Magara <61485115+Keisuke-Magara@users.noreply.github.com>
2022-04-26 12:40:46 +0900

    Fit Button size.

--- lib/Home/UI_home_Button.dart
+++ lib/Home/UI_home_Button.dart
@@ -3,29 +3,33 @@ import 'package:flutter/material.dart';
 /// wrapper import
 import '../Wrapper/wrapper_GetScreenSize.dart';
 
-
 class Button extends StatelessWidget {
   Button({Key? key, required this.buttonName}) : super(key: key);
   String buttonName;
 
   @override
   Widget build(BuildContext context) {
-    GetScreenSize.setSize(MediaQuery.of(context).size.height, MediaQuery.of(context).size.width);
+    GetScreenSize.setSize(
+        MediaQuery.of(context).size.height, MediaQuery.of(context).size.width);
 
     return Container(
       color: Colors.red,
+      width: GetScreenSize.screenWidth() * 0.3,
+      height: GetScreenSize.screenHeight() * 0.1,
       child: Stack(
         children: <Widget>[
           /// asset background screen without image path.
           ///
           /// Image(
-            /// image: AssetImage(""),
+          /// image: AssetImage(""),
           /// ),
           ///
-          Text(
-            buttonName,
-            style: TextStyle(
-              fontSize: GetScreenSize.screenHeight() * 0.1,
+          Center(
+            child: Text(
+              buttonName,
+              style: TextStyle(
+                fontSize: GetScreenSize.screenHeight() * 0.05,
+              ),
             ),
           ),
         ],
