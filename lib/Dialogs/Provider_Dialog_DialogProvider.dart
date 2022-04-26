bb99c4037c58c80de97df56d32fcf2b5d8220385
The-Ishikawa <96249544+The-Ishikawa@users.noreply.github.com>
2022-04-27 02:15:12 +0900

    miner change

--- lib/Dialogs/Provider_Dialog_DialogProvider.dart
+++ lib/Dialogs/Provider_Dialog_DialogProvider.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
 
 class DialogProvider extends ChangeNotifier{
-  double _i = 1.0;
+  double _i = 0.0;
   double get i => _i;
 
-
-
-  Future<void> animatedButton() async {
-    _i = 1.0;
-    await Future.delayed(const Duration(microseconds: 1000));
-    _i = 1.5;
-    notifyListeners();
-    await Future.delayed(const Duration(microseconds: 1000));
+  Future<void> animatedButton() async{
     _i = 1.0;
     notifyListeners();
+
   }
 }
