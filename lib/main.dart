f258e3cf094f083b4f1df2a0a46b683487e52faf
The-Ishikawa <96249544+the-ishikawa@users.noreply.github.com>
2022-05-13 01:48:52 +0900

    miner change

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,73 @@ import 'l10n/l10n.dart';
 
 Future<void> main() async {
   // restrict device screen orientation
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.landscapeLeft,
-    DeviceOrientation.landscapeRight,
-  ]);
-  SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.immersive); // hide Android status bar & navigation bar.
-  await TitleScreen.prepare();
-  runApp(const ProviderScope(child: MyApp()));
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.landscapeLeft,
+  //   DeviceOrientation.landscapeRight,
+  // ]);
+  // SystemChrome.setEnabledSystemUIMode(
+  //     SystemUiMode.immersive); // hide Android status bar & navigation bar.
+  // await TitleScreen.prepare();
+  // runApp(const ProviderScope(child: MyApp()));
+  print("Hello World");
+  int i = 0;
+  Got got = Got();
+  while (i < 10) {
+    got.num(i);
+    i++;
+  }
+  Aironman aironman1 = Aironman(1, 2);
+  Aironman aironman2 = Aironman(100, 1);
+  AdvancedAiroman aironman3 = AdvancedAiroman(1, 4, 400);
+  aironman2.kick();
+  aironman1.panti();
+  aironman3.beam();
+  aironman3.kick();
+}
+
+class Aironman {
+  late int massuru;
+  late int color; //インスタンス変数（クラス内だと呼び出し可能）
+  Aironman(int m, int c) {
+    int massuru;
+    this.massuru = m;
+    this.color = c;
+  }
+  void panti() {
+    print("筋肉量$massuru、色$color,パンチしました。");
+  }
+
+  void kick() {
+    print("筋肉量$massuru,色$color,キックしました。");
+  }
+}
+
+class AdvancedAiroman extends Aironman {
+  late int siryoku;
+  AdvancedAiroman(int m, int c, int s) : super(m, c) {
+    this.siryoku = s;
+  }
+
+  void beam() {
+    print("筋力量${super.massuru},色${super.color},視力${this.siryoku},ビームしました。");
+  }
+
+  //@Override
+  void kick() {
+    super.kick();
+    print("回し蹴りしました。");
+  }
+}
+
+class Got {
+  void num(int g) {
+    if (g % 2 == 0) {
+      print("${g - 1}は偶数です.");
+    } else {
+      print((g - 1).toString() + "は奇数です。");
+    }
+  }
 }
 
 class MyApp extends StatelessWidget {
